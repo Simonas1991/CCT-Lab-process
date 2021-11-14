fb12bc27a3b37f2e4b81d1654313522e44781be3
Simonas1991 <simas.sniuksta@gmail.com>
2021-11-14 19:50:10 +0200

    Final code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
+// libraries
+import React, { useState } from 'react';
+//=================================================//
+
+// custom components
+import Step from './components/Step';
+//=================================================//
+
+// material-ui componenets
+import { Container } from '@material-ui/core';
+//=================================================//
+
+// custom css
 import './App.css';
+//=================================================//
+
+// steps array
+const steps = [
+  {
+    text: 'Build test task',
+    nr: 1,
+    list: ['Create repository', 'Implement designs', 'Implement functionality']
+  },
+  {
+    text: 'Submit your test task',
+    nr: 2,
+    list: ['Open email client', 'Send link with information to careers@cornercasetech.com']
+  },
+  {
+    text: 'Participate in tech interview',
+    nr: 3,
+    list: ['Talk with HR', 'Talk with Tech team']
+  },
+  {
+    text: 'Receive answer',
+    nr: 4,
+    list: ['Receive answers', 'Start your IT career']
+  }
+]
+//=================================================//
 
 function App() {
+  // hooks
+  const [current, setCurrent] = useState('')
+  const [active, setActive] = useState(false)
+  //=================================================//
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="xl">
+      <h1>CCT Lab process</h1>
+      {steps.map((step, i) => (
+        <Step
+          text={step.text}
+          nr={step.nr}
+          list={step.list}
+          id={i}
+          currentId={current}
+          changeCurrent={setCurrent}
+          act={active}
+          changeAct={setActive}
+        />
+      ))}
+    </Container >
   );
 }
 
---
+++ src/components/Step.js
@@ -0,0 +1,131 @@
+// libraries
+import React from 'react';
+//=================================================//
+
+// imported image
+import Image from '../images/Radio.png';
+//=================================================//
+
+// material-ui componenets
+import { Paper, makeStyles } from '@material-ui/core';
+//=================================================//
+
+// material-ui makeStyles
+const useStyles = makeStyles(() => ({
+    div: {
+        marginBottom: '20px'
+    },
+    paragraph: {
+        margin: 'auto 0 auto 36px',
+        fontFamily: 'Manrope',
+        fontStyle: 'normal',
+        fontWeight: 'bold',
+        fontSize: '18px',
+        lineHeight: '25px'
+    },
+    number: {
+        width: '50px',
+        height: '53.58px',
+        background: 'linear-gradient(180deg, #00A9A7 0%, #00BA89 100%)',
+        margin: '18px 0',
+        color: '#FFFFFF',
+        fontFamily: 'Manrope',
+        fontSize: '36px',
+        fontStyle: 'normal',
+        fontWeight: '700',
+        lineHeight: '49px',
+        letterSpacing: '0em',
+        textAlign: 'center'
+    },
+    container: {
+        position: 'relative',
+        padding: '0.5rem 2rem',
+        display: 'flex',
+        height: '85.7215576171875px',
+        '&::after': {
+            position: 'absolute',
+            content: '""',
+            bottom: '0',
+            left: '0',
+            height: '3%',
+            width: '11%',
+            background: 'linear-gradient(180deg, #00A9A7 0%, #00BA89 100%)',
+            transitionDuration: '0.5s',
+        },
+    },
+    containerActive: {
+        position: 'relative',
+        padding: '0.5rem 2rem',
+        display: 'flex',
+        height: '85.7215576171875px',
+        '&::after': {
+            position: 'absolute',
+            content: '""',
+            bottom: '0',
+            left: '0',
+            height: '3%',
+            width: '16.5%',
+            background: 'linear-gradient(180deg, #00A9A7 0%, #00BA89 100%)',
+            transitionDuration: '0.5s',
+        }
+    },
+    paper: {
+        position: 'relative',
+        borderBottom: '',
+        cursor: 'pointer'
+    },
+    li: {
+        marginTop: '10px',
+        fontFamily: 'Roboto',
+        fontSize: '18px',
+        fontStyle: 'normal',
+        fontWeight: 400,
+        lineHeight: '21px',
+        letterSpacing: '0em',
+        textAlign: 'left',
+        listStyleImage: `url(${Image})`
+    }
+}));
+//=================================================//
+
+
+const Step = ({ text, nr, list, id, changeCurrent, currentId, act, changeAct }) => {
+    // functions
+    const changeStatus = () => {
+        changeCurrent(id)
+        if (act === false) {
+            changeAct(true)
+        }
+        else if (act && currentId === id) {
+            changeAct(false)
+        }
+    }
+    //=================================================//
+
+    // classes declaration
+    const classes = useStyles();
+    //=================================================//
+
+    return (
+        <div className={classes.div}>
+            <Paper elevation={6} className={classes.paper} onClick={changeStatus}>
+                <div className={act && currentId === id ? classes.containerActive : classes.container}>
+                    <div className={classes.number}>{nr}</div>
+                    <p className={classes.paragraph}>{text}</p>
+                </div>
+            </Paper>
+            {
+                currentId === id && act ?
+                    <ul>
+                        {list.map((listItem, i) => (
+                            <li className={classes.li} key={listItem[i]}>{listItem}</li>
+                        ))}
+                    </ul>
+                    :
+                    <></>
+            }
+        </div>
+    )
+}
+
+export default Step
